5801fda23c30d1ae7d549f888f4bee2363b16ac3
Sardar ali <alisardar465@gmail.com>
2021-10-18 12:51:27 +0500

    comment homePage.jpg image because show errors in the project start.

--- src/sections/Home/HomeHeader/HomeHeader.jsx
+++ src/sections/Home/HomeHeader/HomeHeader.jsx
@@ -2,17 +2,17 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import logo from "./../../../assets/images/agslogo/Logo (1).png";
-import header_bgMB from "./../../../assets/images/agsbanners/HomePage.JPG";
+// import header_bgMB from "./../../../assets/images/agsbanners/HomePage.JPG";
 import header_bg from "./../../../assets/images/agsbanners/homepageBanner.png";
 // import AGSPrincipalWelcomeMessage from "./../../../assets/images/bannerVideo/PrincipalWelcomeMessage.mp4";
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import ModalVideo from 'react-modal-video'
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import ModalVideo from "react-modal-video";
 import Hidden from "@material-ui/core/Hidden";
 import BackGroundVideo from "../../../components/BackGroundVideo/BackGroundVideo";
 import PauseIcon from "@material-ui/icons/Pause";
 
 function HomeHeader(props) {
-  const [isOpen, setOpen] = useState(false)
+  const [isOpen, setOpen] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   // const [isVideo, setIsVideo] = useState(false);
   const togglePlay = () => {
@@ -23,12 +23,12 @@ function HomeHeader(props) {
       // setIsVideo(true);
     } else {
       myVideo.pause();
-      myVideo.load()
+      myVideo.load();
       // setIsVideo(false);
     }
     // update the state
     setIsPlaying(!isPlaying);
-  }
+  };
 
   // const videoSource = "https://american-gulf-school.b-cdn.net/videos/PrincipalWelcomeMessage.mp4";
 
@@ -37,11 +37,16 @@ function HomeHeader(props) {
   return (
     <>
       <Hidden smDown>
-        <div style={{ height: '', backgroundImage: `url(${isPlaying && props?.bannerImg})` }} className={isPlaying ? "videoBottom" : "BackgroundDiv"}>
+        <div
+          style={{
+            height: "",
+            backgroundImage: `url(${isPlaying && props?.bannerImg})`,
+          }}
+          className={isPlaying ? "videoBottom" : "BackgroundDiv"}>
           <BackGroundVideo
             // blur={2}
             bannerImg={props.bannerImg}
-          // videoSource={videoSource}
+            // videoSource={videoSource}
           >
             <div className='content'>
               {/*<div className='sub-content' >*/}
@@ -50,34 +55,35 @@ function HomeHeader(props) {
                   <Row>
                     <Col sm={5}>
                       {!isPlaying ? (
-                        <div className="description-column">
-                          <div className="header-logo-wrapper">
+                        <div className='description-column'>
+                          <div className='header-logo-wrapper'>
                             {/* <img src={logo} alt="AGS Logo" className="header-logo" /> */}
                           </div>
-                          <h1 className="intro-title">
+                          <h1 className='intro-title'>
                             {props.banner?.title}
                             {/* AMERICAN GULF SCHOOL */}
                           </h1>
-                          <p className={"intro-title2"} >
+                          <p className={"intro-title2"}>
                             {props.banner?.subtitle}
                             {/* EDUCATION DONE RIGHT */}
                           </p>
-                          <p className={"subtitle"}
+                          <p
+                            className={"subtitle"}
                             dangerouslySetInnerHTML={{
-                              __html:
-                                props.banner?.description,
-                            }}
-                          >
+                              __html: props.banner?.description,
+                            }}>
                             {/* Take a look at what the CEO of the school
                             <br />
                             has to say. */}
                           </p>
                         </div>
-                      ) : ""}
+                      ) : (
+                        ""
+                      )}
                     </Col>
                     <Hidden smDown>
                       <Col sm={{ span: 6 }} className={"iconSpace"}>
-                        <div className="video-promo-content">
+                        <div className='video-promo-content'>
                           {/* <button className="btn About-video-play-icon"
                             onClick={togglePlay}
                           >
@@ -98,17 +104,26 @@ function HomeHeader(props) {
         </div>
       </Hidden>
       <Hidden mdUp>
-        <div className="home-header"
-          style={{ backgroundImage: `url(${header_bgMB})` }}
+        <div
+          className='home-header'
+          // style={{ backgroundImage: `url(${header_bgMB})` }}
         >
           <div className={"inner-header flex"}>
             <Container>
               <Row>
                 <Hidden smDown>
                   <Col sm={{ span: 6 }} className={"iconSpace"}>
-                    <div className="video-promo-content">
-                      <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="RBZutrFFhTA" onClose={() => setOpen(false)} />
-                      <button className="btn About-video-play-icon" onClick={() => setOpen(true)}>
+                    <div className='video-promo-content'>
+                      <ModalVideo
+                        channel='youtube'
+                        autoplay
+                        isOpen={isOpen}
+                        videoId='RBZutrFFhTA'
+                        onClose={() => setOpen(false)}
+                      />
+                      <button
+                        className='btn About-video-play-icon'
+                        onClick={() => setOpen(true)}>
                         <PlayArrowIcon className={"AboutVideoplaySize"} />
                       </button>
                     </div>
